d11ee906a78235896b946db5861cc117002d6f92
ric3500 <47509361+ric3500@users.noreply.github.com>
2019-02-10 22:29:03 -0600

    Create torrehanoi.cpp

---
+++ torrehanoi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+	system("cls");
+	cout << "hola mundo" << endl;
+	system("pause");
+	return 0;
+}

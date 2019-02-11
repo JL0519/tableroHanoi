5de9748e18c01eda71010336a33cc7bb633c32d9
ric3500 <47509361+ric3500@users.noreply.github.com>
2019-02-10 23:39:42 -0600

    Update torrehanoi.cpp

--- torrehanoi.cpp
+++ torrehanoi.cpp
@@ -1,4 +1,4 @@
-// prueba proyecto.
+// prueba proyecto 1.
 
 #include <iostream>
 #include <conio.h>
@@ -6,10 +6,23 @@
 
 using namespace std;
 
+int menu();
+
 int main()
 {
 	system("cls");
-	cout << "hola mundo" << endl;
-	system("pause");
+	menu();
 	return 0;
+}
+
+int menu()
+{
+	system("cls");
+	cout << " .: Bivenvenido al juego Torres de Hanoi:. /n/n";
+	cout << "######### Menu ##########";
+	cout << "1. Iniciar Juego. " << endl;
+	cout << "2. ver reglas del juego." << endl;
+	cout << "3. Salir." << endl;
+	cout << "#########################" << endl;
+
 }

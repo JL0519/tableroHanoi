bd5140b8734346cf3c8dae61b88a9c8f59d8a14f
jl051 <jl051@192.168.0.30>
2019-02-12 13:12:13 -0600

    Menu con do-while

---
+++ progra1_proyecto1/src/proy1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int menu();
+
+int main()
+{
+	system("cls");
+	menu();
+	return 0;
+}
+
+int menu()
+{
+	int opcion;
+
+	system("cls");
+	cout << " .: Bivenvenido al juego Torres de Hanoi:."<<endl;
+
+	do{
+	cout << "\n######### Menu ##########"<<endl;
+	cout << "1. Ver reglas del juego.. " << endl;
+	cout << "2. Iniciar Juego." << endl;
+	cout << "3. Salir." << endl;
+	cout << "#########################" << endl;
+
+	cin>>opcion;
+
+	switch(opcion){
+
+	case 1: cout<<"-------------------Introducir reglas del juego--------------------------------"; break;
+
+	case 2: cout<<"---------------------codigo del juego-----------------------------------------"; break;
+
+	//default: cout<<"Opcion no valida!!!!!"; break;
+	}
+
+	}while(opcion!=3);
+
+	cout<<"Gracias por jugar!!!!!";
+
+}
+
+
+
+

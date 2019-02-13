84e2d9205c17f4e94a33e7378d38acb1f37038ce
luixgarcia11 <47509981+luixgarcia11@users.noreply.github.com>
2019-02-13 14:36:17 -0600

    Update proy1.cpp

--- progra1_proyecto1/src/proy1.cpp
+++ progra1_proyecto1/src/proy1.cpp
@@ -31,7 +31,12 @@ int menu()
 
 	switch(opcion){
 
-	case 1: cout<<"-------------------Introducir reglas del juego--------------------------------"; break;
+	case 1: cout<<"Se colocan X (cantidad) de discos en uno de los ejes o columnas, en forma decreciente, con el disco mayor en la base"<<endl; 
+cout<<"El juego consiste en trasladar los discos de dicho eje a cualquiera de los dos restantes"<<endl;
+cout<<"pudiéndose utilizar los tres ejes indistintamente a lo largo de todo el desarrollo del juego,"<<endl; 
+cout<<"todo esto con movimientos sucesivos, pero teniendo en cuenta que solamente deberá moverse un disco por vez,"<<endl; 
+cout<<"estando prohibido colocar un disco de mayor diámetro sobre uno de menor diametro, se debe realizar el cambio"<<endl;
+cout<<"con la menor cantidad de movimientos posibles"; break;
 
 	case 2: cout<<"---------------------codigo del juego-----------------------------------------"; break;
 
